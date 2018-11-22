e37ed1238547dd0321a49b7476b8fa2d4d146dd9
chethankailash <chethankailash3@gmail.com>
2018-11-22 19:48:30 +0530

    mlab and db configured

--- config/db.js
+++ config/db.js
@@ -1,7 +1,8 @@
 const mongoose = require('mongoose');
 mongoose.Promise = global.Promise;
+const CONNECTION_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/mcaticketmaster' 
 
-mongoose.connect('mongodb://localhost:27017/mcaticketmaster', {
+mongoose.connect(CONNECTION_URI, {
     useNewUrlParser: true
 });
 
